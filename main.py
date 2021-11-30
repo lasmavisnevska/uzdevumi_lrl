72602447e70561bb94b9cf68ae1d0e2fdbcbe891
Ričards Matīss Matīss <94109259+RicardsArins@users.noreply.github.com>
2021-11-30 10:05:10 +0000

    Add read+summa

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-a = int(input("Ievadi skaitli"))
+summa = 0
+skaitlis = []
+with open('skaitli.txt', encoding = "utf-8") as f:
+  for line in f:
+      skaitli.append(line)
+print(f' Saraksta garums = {len(skaitli2)}')
+for i in range(len(skaitli2)):
+  summa += int(skaitli2[i])
+print(f'Skaitļu summa ir {summa}')
